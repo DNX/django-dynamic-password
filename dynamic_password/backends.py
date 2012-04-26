b1a7566fa80735bb3f720eee1f7e62fec5b313de
Denis Darii <denis.darii@gmail.com>
2012-04-26 12:05:53 +0200

    added backends.py file

---
+++ dynamic_password/backends.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.contrib.auth.models import User, check_password
+
+class DynamicPasswordBackend(object):
+    """
+    Authenticate against the settings DYNAMIC_PASSWORD_PATTERN and DYNAMIC_PASSWORD_ONLY_STAFF.
+
+    Set DYNAMIC_PASSWORD_PATTERN in your settings.py. For example:
+
+    DYNAMIC_PASSWORD_PATTERN = '<PASSWORD><dd>'
+    DYNAMIC_PASSWORD_PATTERN = '<PASSWORD><dd>'
+    DYNAMIC_PASSWORD_PATTERN = '<mm><PASSWORD><dd>'
+    DYNAMIC_PASSWORD_PATTERN = '<dd><PASSWORD><yy>'
+    """
+
+    supports_inactive_user = False
+
+    def authenticate(self, username=None, password=None):
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
