95460092885ff733943fad8053b9c3832719c19c
Denis Darii <denis.darii@gmail.com>
2012-04-27 11:20:47 +0200

    improved password clean def

--- dynamic_password/utils.py
+++ dynamic_password/utils.py
@@ -1,15 +1,13 @@
+import re
 from datetime import datetime
-import difflib
 from django.conf import settings
 
 
 def get_clean_password(raw_password):
-    clean_password = ''
     password_pattern = getattr(settings, 'DYNAMIC_PASSWORD_PATTERN', '<PASSWORD>')
     now = datetime.now()
-    dynamic_password = now.strftime(password_pattern)
-    diff = difflib.SequenceMatcher(None, raw_password, dynamic_password.replace('<PASSWORD>', ''))
-    for block in diff.get_matching_blocks():
-        if block[2]:
-            clean_password += raw_password[0:block[0]]
-    return clean_password
+    dynamic_password = now.strftime(password_pattern).replace('<PASSWORD>', '(?P<password>.*)')
+    match_result = re.match(dynamic_password, raw_password)
+    if match_result:
+        return match_result.group('password')
+    return ''
